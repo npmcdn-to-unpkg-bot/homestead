0813ec5ab2cb5ff704e5f31a5cda073ded570b87
nowarena <nowarena@github.com>
2016-08-08 21:15:58 -0700

    moved game logic into separate file

---
+++ public_html/js/match.js
@@ -0,0 +1,113 @@
+var globalObj = {
+
+    numPieces: 8,
+    score: 0,
+    matches: 0,
+    misses: 0,
+    visibleJArr: [],
+    completedArr: [],
+    visibleIndexArr: [],
+    completedArr: [],
+
+    getScore: function()
+    {
+        if (this.matches == 0 || this.misses == 0 ) {
+            this.score = 0;
+        } else {
+            this.score = parseInt((this.matches / this.misses) * 100) + '%';
+        }
+    },
+    isValidClick: function(index)
+    {
+        // don't compute clicks on already completed squares
+        for(var key in this.completedArr) {
+            if (typeof this.completedArr[key] != 'undefined' && this.completedArr[key] == this.props.index) {
+                return false;
+            }
+        }
+
+        // if they've clicked on the same square twice
+        if (this.visibleIndexArr.length == 1 && index == this.visibleIndexArr[0]) {
+            return false;
+        }
+
+        return true;
+    },
+    eval: function(props)
+    {
+        this.visibleJArr.push(props.j);
+        this.visibleIndexArr.push(props.index);
+        if (this.visibleJArr.length == 2) {
+            if (this.visibleJArr[0] == this.visibleJArr[1]) {
+                this.matches++;
+                this.completedArr.push(props.index);
+                var result = 'match';
+            } else {
+                this.misses++;
+                var result = 'miss';
+            }
+
+            this.visibleJArr = new Array();
+            this.visibleIndexArr = new Array();
+
+            if (this.numPieces == this.completedArr.length / 2) {
+                this.msg = "Game Over. <a href=''>Play again.</a>";
+            }
+
+            return result;
+        }
+
+        return 'oneclicked';
+    },
+    shuffle: function(array) {
+
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    },
+    getGrid:function () {
+
+        // Build the grid
+        // Set an array with a sequential range of numbers
+        var gridArr = [];
+        var numArr = [];
+        for(var j = 1; j <= this.numPieces; j++) {
+            numArr.push(j);
+        }
+
+        // Loop over the array twice, randomizing the order each time. Store the randomized array
+        var index = 0;
+        for(var i = 1; i <= 2; i++) {
+            numArr = this.shuffle(numArr);
+            for(var x in numArr) {
+                ++index;
+                j = numArr[x];
+                var obj = {
+                    index: index,
+                    indexKey: index,
+                    i: i,
+                    j:j,
+                    displayJ:j
+                };
+                gridArr.push(obj);
+            }
+        }
+
+        return gridArr;
+
+    }
+
+};
--- resources/views/game/reactmatch.php
+++ resources/views/game/reactmatch.php
@@ -8,134 +8,16 @@
     <script src="https://npmcdn.com/react-dom@15.3.0/dist/react-dom.js"></script>
     <script src="https://npmcdn.com/babel-core@5.8.38/browser.min.js"></script>
     <script src="https://npmcdn.com/jquery@3.1.0/dist/jquery.min.js"></script>
-
+    <script src="/js/match.js"></script>
     <link href="/css/match.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="content"></div>
 
-<script>
+<script type="text/babel">
 
     var prevVisibleComponent = '';
     var curVisibleComponent = '';
-
-</script>
-
-<script type="text/babel">
-
-    var globalObj = {
-
-        numPieces: 8,
-        score: 0,
-        matches: 0,
-        misses: 0,
-        visibleJArr: [],
-        completedArr: [],
-        visibleIndexArr: [],
-        completedArr: [],
-
-        getScore: function()
-        {
-            if (this.matches == 0 || this.misses == 0 ) {
-                this.score = 0;
-            } else {
-                this.score = parseInt((this.matches / this.misses) * 100) + '%';
-            }
-        },
-        isValidClick: function(index)
-        {
-            // don't compute clicks on already completed squares
-            for(var key in this.completedArr) {
-                if (typeof this.completedArr[key] != 'undefined' && this.completedArr[key] == this.props.index) {
-                    return false;
-                }
-            }
-
-            // if they've clicked on the same square twice
-            if (this.visibleIndexArr.length == 1 && index == this.visibleIndexArr[0]) {
-                return false;
-            }
-
-            return true;
-        },
-        eval: function(props)
-        {
-            this.visibleJArr.push(props.j);
-            this.visibleIndexArr.push(props.index);
-            if (this.visibleJArr.length == 2) {
-                if (this.visibleJArr[0] == this.visibleJArr[1]) {
-                    this.matches++;
-                    this.completedArr.push(props.index);
-                    var result = 'match';
-                } else {
-                    this.misses++;
-                    var result = 'miss';
-                }
-
-                this.visibleJArr = new Array();
-                this.visibleIndexArr = new Array();
-
-                if (this.numPieces == this.completedArr.length / 2) {
-                    this.msg = "Game Over. <a href=''>Play again.</a>";
-                }
-
-                return result;
-            }
-
-            return 'oneclicked';
-        },
-        shuffle: function(array) {
-
-            var currentIndex = array.length, temporaryValue, randomIndex;
-
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
-
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
-
-                // And swap it with the current element.
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
-            }
-
-            return array;
-        },
-        getGrid:function () {
-
-            // Build the grid
-            // Set an array with a sequential range of numbers
-            var gridArr = [];
-            var numArr = [];
-            for(var j = 1; j <= this.numPieces; j++) {
-                numArr.push(j);
-            }
-
-            // Loop over the array twice, randomizing the order each time. Store the randomized array
-            var index = 0;
-            for(var i = 1; i <= 2; i++) {
-                numArr = this.shuffle(numArr);
-                for(var x in numArr) {
-                    ++index;
-                    j = numArr[x];
-                    var obj = {
-                        index: index,
-                        indexKey: index,
-                        i: i,
-                        j:j,
-                        displayJ:j
-                    };
-                    gridArr.push(obj);
-                }
-            }
-
-            return gridArr;
-
-        }
-
-    };
 
     var Box = React.createClass({
 
@@ -199,12 +81,10 @@ console.log('miss');
         },
 
         render: function() {
-
             var style = 'display:none';
             if (this.props.clickedIndex == this.props.index) {
                 style = 'display:block';
             }
-
             return (
                 <div className="Box" onClick={this.handleOnClick}>
                     <div style={{style}} className="InnerBox">{this.props.displayJ}</div>
@@ -240,29 +120,7 @@ console.log('miss');
     });
 
     var Header = React.createClass({
-        // does nothing, i have to pass in values in Game component init
-//        getInitialState: function() {
-//            console.log('getInitialState');
-//            //return {data:[]};
-//            return {misses: 0, matches:0};
-//        },
-//        componentDidMount: function() {
-//            console.log(' header componentDidMount');
-//            window.addEventListener('click', this.handleHeaderData);
-//
-//        },
-//        componentWillUnmount: function() {
-//            window.removeEventListener('click', this.handleHeaderData);
-//        },
-//        handleHeaderData: function(e) {
-//
-//            this.setState({});// triggers a re-rendering, don't need to set anything
-//
-//        },
-
         render: function() {
-console.log("header", globalObj);
-
             return (
                 <div ref="Header" className="Header">
                     <h3>
@@ -277,7 +135,6 @@ console.log("header", globalObj);
     });
 
     var Footer = React.createClass({
-
         render: function() {
             return (
                 <div className="Footer">
@@ -291,7 +148,6 @@ console.log("header", globalObj);
     });
 
     var Game = React.createClass({
-
         render: function() {
             return (
                 <div className="Game">
